d48a391792d9143c920ad37513f5db81df469ce2
awoladhossain <awoladh04@gmail.com>
2025-11-17 16:53:33 +0600

    updated

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,16 +1,21 @@
+import bcrypt from "bcrypt";
 import { StatusCodes } from "http-status-codes";
 import User from "../models/user.model.js";
 import AppError from "../utils/AppError.js";
 import { catchAsync } from "../utils/catchAsync.js";
 import { successResponse } from "../utils/response.js";
 
-export const register = catchAsync(async (req, res, next) => {
+
+
+export const registerUser = catchAsync(async (req, res, next) => {
   const { fullname, email, password } = req.body;
   const existingUser = await User.findOne({ email });
   if (existingUser) {
     return next(new AppError("User already exists", StatusCodes.BAD_REQUEST));
   }
-  const user = await User.create({ fullname, email, password });
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({ fullname, email, password: hashedPassword });
+
   return successResponse(
     res,
     StatusCodes.CREATED,
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
+import morgan from "morgan";
 import connectDB from "./database/db.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import authRouters from "./routes/auth.routes.js";
 const PORT = process.env.PORT || 6000;
 const app = express();
 app.use(cookieParser());
@@ -14,8 +16,17 @@ app.use(
   })
 );
 // routes
+app.get("/", (req, res) => {
+  res.send("Hello from backend");
+})
+app.use("/api/auth", authRouters);
+
 // global error handler
 app.use(errorMiddleware);
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 connectDB();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
