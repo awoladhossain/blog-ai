fdd8896e5041da0efcd73de1817276b9afb8e77b
awoladhossain <awoladh04@gmail.com>
2025-11-30 13:30:39 +0600

    updated the controller for blog

--- server/controllers/blog.controller.js
+++ server/controllers/blog.controller.js
@@ -5,6 +5,7 @@ import Blog from "../models/blog.model.js";
 import AppError from "../utils/AppError.js";
 import { catchAsync } from "../utils/catchAsync.js";
 import { successResponse } from "../utils/response.js";
+import { StatusCodes } from "http-status-codes";
 
 export const addBlog = catchAsync(async (req, res, next) => {
   const data = JSON.parse(req.body.data || "{}");
@@ -40,7 +41,7 @@ export const addBlog = catchAsync(async (req, res, next) => {
   });
 
   const newBlog = await blog.save();
-  return successResponse(res, 201, "Blog created", newBlog);
+  return successResponse(res, StatusCodes.CREATED, "Blog created", newBlog);
 });
 
 export const showAllBlogs = catchAsync(async (req, res, next) => {
@@ -50,5 +51,107 @@ export const showAllBlogs = catchAsync(async (req, res, next) => {
     .sort({ createdAt: -1 })
     .lean()
     .exec();
-  return successResponse(res, 200, "Blogs found successfully", blogs);
+  return successResponse(res, StatusCodes.OK, "Blogs found successfully", blogs);
+});
+
+export const getBlogById = catchAsync(async (req, res, next) => {
+  const id = req.params.id;
+  const blog = await Blog.findById(id)
+    .populate("author", "fullname")
+    .populate("category", "name")
+    .lean()
+    .exec();
+  if (!blog) {
+    return next(new AppError("Blog not found", StatusCodes.NOT_FOUND));
+  }
+  return successResponse(res, StatusCodes.OK, "Blog found successfully", blog);
+})
+
+export const editBlog = catchAsync(async (req, res, next) => {
+  const id = req.params.id;
+
+  // Find the existing blog
+  const blog = await Blog.findById(id);
+  if (!blog) {
+    return next(new AppError("Blog not found", StatusCodes.NOT_FOUND));
+  }
+
+  // Parse the JSON data from the form
+  const data = JSON.parse(req.body.data || "{}");
+
+  // Prepare update object
+  const updateData = {
+    category: data.category,
+    title: data.title,
+    slug: data.slug,
+    author: data.author,
+  };
+
+  // Handle description encoding
+  if (data.description) {
+    const descriptionText = data.description ?? "";
+    const safeDescription = encode(String(descriptionText));
+    updateData.description = safeDescription;
+  }
+
+  // If a new image was uploaded, handle it
+  if (req.file) {
+    try {
+      // Delete old image from Cloudinary (optional but recommended)
+      if (blog.featuredImage) {
+        const urlParts = blog.featuredImage.split("/");
+        const publicIdWithExtension = urlParts[urlParts.length - 1];
+        const publicId = publicIdWithExtension.split(".")[0];
+        await cloudinary.uploader.destroy(`blog-image/${publicId}`);
+      }
+
+      // Upload new image using streamifier
+      const result = await new Promise((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+          { folder: "blog-image" },
+          (error, data) => (error ? reject(error) : resolve(data))
+        );
+        streamifier.createReadStream(req.file.buffer).pipe(uploadStream);
+      });
+
+      updateData.featuredImage = result.secure_url;
+    } catch (uploadError) {
+      return next(new AppError("Failed to upload image", 500));
+    }
+  }
+
+  // Update the blog
+  const updatedBlog = await Blog.findByIdAndUpdate(id, updateData, {
+    new: true, // Return the updated document
+    runValidators: true, // Run schema validators
+  })
+    .populate("category", "name")
+    .populate("author", "fullname")
+    .lean()
+    .exec();
+
+  return successResponse(
+    res,
+    StatusCodes.OK,
+    "Blog updated successfully",
+    updatedBlog
+  );
+});
+
+export const deleteBlog = catchAsync(async (req, res, next) => {
+  const id = req.params.id;
+  console.log("the error: ", id);
+  const blog = await Blog.findByIdAndDelete(id);
+
+  if (!blog) {
+    return next(new AppError("Blog not found", StatusCodes.NOT_FOUND));
+  }
+
+  // Return the deleted blog
+  return successResponse(
+    res,
+    StatusCodes.OK,
+    "Blog deleted successfully",
+    blog
+  );
 });
--- server/routes/blog.routes.js
+++ server/routes/blog.routes.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { addBlog, showAllBlogs } from "../controllers/blog.controller.js";
+import {
+  addBlog,
+  deleteBlog,
+  editBlog,
+  getBlogById,
+  showAllBlogs,
+} from "../controllers/blog.controller.js";
 import { upload } from "../middlewares/upload.middleware.js";
 
 const route = express.Router();
 
 route.post("/add", upload.single("avatar"), addBlog);
 route.get("/", showAllBlogs);
+route.get("/:id", getBlogById);
+route.put("/blog-edit/:id", upload.single("avatar"), editBlog);
+route.delete("/:id", deleteBlog);
 
 export default route;
